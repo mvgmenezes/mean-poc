aa8a9f69ee62dabf031ccdddae774e2c880dc567
Marcus Menezes <mvgmenezes@gmail.com>
2019-02-11 15:38:12 -0200

    Incluindo o Model/Controller e Rota, usando o comando: yo meanjs:express-model category / yo meanjs:express-controller categories / yo meanjs:express-route categories

---
+++ modules/category/server/controllers/category.server.controller.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var path = require('path'),
+  mongoose = require('mongoose'),
+  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
+  _ = require('lodash');
+
+/**
+ * Create a Category
+ */
+exports.create = function (req, res) {
+
+};
+
+/**
+ * Show the current Category
+ */
+exports.read = function (req, res) {
+
+};
+
+/**
+ * Update a Category
+ */
+exports.update = function (req, res) {
+
+};
+
+/**
+ * Delete an Category
+ */
+exports.delete = function (req, res) {
+
+};
+
+/**
+ * List of Categories
+ */
+exports.list = function (req, res) {
+
+};
